12e42243b78ba9724e9d892971ce815ddcbc257d
santhosh thota <santhosh2011@gmail.com>
2022-07-31 10:17:32 +0530

    Adding mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [MongooseModule.forRoot(process.env.mongourl)],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
+        MongooseModule.forRoot(process.env.mongourl)
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
+
 export class AppModule {
 }
